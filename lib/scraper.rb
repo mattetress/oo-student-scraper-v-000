825e7fdf7c6098dfa435e43d82a72c0d3b278531
Matt Etress <mattetress@me.com>
2018-10-14 03:09:25 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,7 +26,7 @@ class Scraper
         profile_info[media_type.to_sym] = media_link
       end
     end
-    
+
     profile_info[:profile_quote] = doc.css("div.profile-quote").text
     profile_info[:bio] = doc.css("div.description-holder p").text
     
--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-
+    student_hash.each do { |k, v| self.send("#{k}=", v) }
   end
 
   def self.create_from_collection(students_array)
